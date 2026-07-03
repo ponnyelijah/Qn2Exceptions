b04d63a94846bfb785f758e306a86bc1bd722aea
ponnyelijah <ponnyelijah@gmail.com>
2026-07-03 14:34:00 +0300

    Create FileHandler.java

---
+++ FileHandler.java
@@ -0,0 +1,18 @@
+package isaacwandyakaortis.qn2exceptions;
+    import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileHandler {
+
+    public static void readFile() {
+        // The resource is opened within the try statement parentheses
+        try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+            String line = br.readLine();
+            System.out.println(line);
+        } catch (IOException e) {
+            System.err.println("An error occurred while reading the file: " + e.getMessage());
+        }
+    }
+}
+    
